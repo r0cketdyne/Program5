f545660fc5d0d173eeaaf94cdad2931a945272ba
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-04-25 02:39:42 +0300

    histooo

--- Program5.py
+++ Program5.py
@@ -4,9 +4,15 @@
 #this prog reads a list of grades, computes the average grade, the median grade, the high and low
 #grades, and print a histogram where each * represents one of the counted grade types.
 #################################################################################################
+#Matthew Stephenson
+#04/20/2024
+#Program 5
+#this prog reads a list of grades, computes the average grade, the median grade, the high and low
+#grades, and print a histogram where each * represents one of the counted grade types.
+#################################################################################################
 def grade_counter(grades):
     """
-    This func takes a list of grades and returns a list of counts for each grade range (A-F).
+    This function takes a list of grades and returns a list of counts for each grade range (A-F).
     """
     grade_counts = [0] * 5
     for grade in grades:
@@ -16,29 +22,35 @@ def grade_counter(grades):
 
 def compute_stats(grades):
     """
-    This func computes the average, median, highest, and lowest grades from the given list of grades.
+    This function computes the average, median, highest, and lowest grades from the given list of grades.
     """
     sorted_grades = sorted(grades)
     grade_counts = grade_counter(sorted_grades)
     total = sum(sorted_grades)
     average = total / len(sorted_grades)
-    median = sorted_grades[len(sorted_grades) // 2]
+    median_index = len(sorted_grades) // 2
+    median = (sorted_grades[median_index] + sorted_grades[~median_index]) / 2
     highest = max(sorted_grades)
     lowest = min(sorted_grades)
     return grade_counts, average, median, highest, lowest
 
 def print_histogram(grades):
     """
-    This func logs the computed statistics and a histogram of the grades to the standard i/o
+    This function logs the computed statistics and a histogram of the grades to the standard i/o
     """
-    grade_counts, average, median, highest, lowest = compute_stats(grades)
-    print("Average:", average)
-    print("Median:", median)
-    print("Highest:", highest)
-    print("Lowest:", lowest)
+    grade_counts, _, _, _, _ = compute_stats(grades)
+    print(f"{len(grades)} grades were read in.")
+    print("Average:", "{:.2f}".format(compute_stats(grades)[1]))
+    print("Median:", "{:.2f}".format(compute_stats(grades)[2]))
+    print("Highest:", "{:.2f}".format(compute_stats(grades)[3]))
+    print("Lowest:", "{:.2f}".format(compute_stats(grades)[4]))
     print("HISTOGRAM")
-    for grade in range(5):
-        print(f"{chr(65 + grade)}: {'*' * grade_counts[grade]}")
+    for i, count in enumerate(grade_counts):
+        grade_letter = chr(65 + i)
+        if i < 3:
+            print(f"{grade_letter}: {'*' * count}")
+        else:
+            print(f"{grade_letter}: {'*' * count}")
 
 grades = []
 while True:
@@ -47,4 +59,4 @@ while True:
         break
     grades.append(int(grade))
 
-print_histogram(grades)
+print_histogram(grades)100
